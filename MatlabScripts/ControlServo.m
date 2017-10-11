5f8226a9f296b9381bb4c9a592fa38f0dea19e5f
jaspereb <jasperebrown@gmail.com>
2017-10-11 11:15:43 +1100

    moved files

---
+++ MatlabScripts/ControlServo.m
@@ -0,0 +1,44 @@
+%This program connects to the openCM9 over serial and moves it around to
+%demonstrate that it is connected
+
+if(~exist('s'))
+    s = serial('COM7');
+    fopen(s);
+    disp('Opening Serial');
+end
+
+
+fprintf(s, 'a1');
+fprintf(s, 'p0');
+pause(2);
+fprintf(s, 'p1600');
+pause(2);
+
+closeTo = 1600;
+
+closeHand(s, closeTo);
+pause(1);
+closeHand(s, 0);
+
+% %Do some stuff
+% fprintf(s, 'a2');
+% fprintf(s, 'p%d\n', closeTo);
+% 
+% fprintf(s, 'a3');
+% fprintf(s, 'p%d\n', closeTo);
+% 
+% fprintf(s, 'a4');
+% fprintf(s, 'p%d\n', closeTo);
+% 
+% pause(5);
+% 
+% fprintf(s, 'a2');
+% fprintf(s, 'p0');
+% 
+% fprintf(s, 'a3');
+% fprintf(s, 'p0');
+% 
+% fprintf(s, 'a4');
+% fprintf(s, 'p0');
+% 
+% pause(1);
